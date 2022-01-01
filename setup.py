aa7910658e547e911cb138b3d4a75255d88df435
Marco <6640057+martinellimarco@users.noreply.github.com>
2022-01-01 18:10:13 +0100

    New setup.py that should fix #2 and #3. Tested with GCC 11 and Clang 11 to 13 on Linux. Tested with Clang 12 on MacOS

--- setup.py
+++ setup.py
@@ -9,37 +9,22 @@ from sys import platform
 
 buildCython = '--cython' in sys.argv
 
-if platform == "darwin":
-    extensions = [
-        Extension(
-            name               = 'indexed_zstd',
-            sources            = [ 'indexed_zstd/indexed_zstd.pyx' ] if buildCython
-                                else [ 'indexed_zstd/indexed_zstd.cpp' ],
-            include_dirs       = [ '.' ],
-            language           = 'c++',
-            extra_compile_args = [ '-std=c++11', '-O3', '-DNDEBUG' ],
-            libraries = ['m', 'zstd'],
-        ),
-    ]
-
-    zstd_seek = ('zstd_zeek', {
-        'sources': ['indexed_zstd/libzstd-seek/zstd-seek.c']
-    })
-else:
-    extensions = [
-        Extension(
-            name               = 'indexed_zstd',
-            sources            = [ 'indexed_zstd/indexed_zstd.pyx', 'indexed_zstd/libzstd-seek/zstd-seek.c' ] if buildCython
-                                else [ 'indexed_zstd/indexed_zstd.cpp', 'indexed_zstd/libzstd-seek/zstd-seek.c' ],
-            include_dirs       = [ '.' ],
-            language           = 'c++',
-            extra_compile_args = [ '-std=c++11', '-O3', '-DNDEBUG' ],
-            libraries = ['m', 'zstd'],
-        ),
-    ]
+extensions = [
+    Extension(
+        name               = 'indexed_zstd',
+        sources            = [ 'indexed_zstd/indexed_zstd.pyx' ] if buildCython
+                            else [ 'indexed_zstd/indexed_zstd.cpp' ],
+        include_dirs       = [ '.' ],
+        language           = 'c++',
+        extra_compile_args = [ '-std=c++11', '-O3', '-DNDEBUG', '-stdlib=libc++', '-mmacosx-version-min=10.9' ] if platform == "darwin"
+                             else [ '-std=c++11', '-O3', '-DNDEBUG' ],
+        extra_link_args=[ '-lzstd', '-stdlib=libc++', '-mmacosx-version-min=10.9' ] if platform == "darwin" else [ '-lzstd' ],
+        libraries = [ 'm' ],
+    ),
+]
 
 zstd_seek = ('zstd_zeek', {
-    'sources': ['indexed_zstd/libzstd-seek/zstd-seek.c']
+    'sources': [ 'indexed_zstd/libzstd-seek/zstd-seek.c' ]
 })
 
 if buildCython:
@@ -51,16 +36,17 @@ scriptPath = os.path.abspath( os.path.dirname( __file__ ) )
 with open( os.path.join( scriptPath, 'README.md' ), encoding = 'utf-8' ) as file:
     readmeContents = file.read()
 
+
 setup(
     name             = 'indexed_zstd',
-    version          = '1.3.1',
+    version          = '1.3.2',
 
     description      = 'Fast random access to zstd files',
     url              = 'https://github.com/martinellimarco/indexed_zstd',
     author           = 'Martinelli Marco with help of Maximilian Knespel',
     license          = 'MIT',
     classifiers      = [ 'License :: OSI Approved :: MIT License',
-                         'Development Status :: 4 - Beta',
+                         'Development Status :: 5 - Production/Stable',
                          'Operating System :: POSIX',
                          'Operating System :: Unix',
                          'Programming Language :: Python :: 3',
@@ -70,6 +56,7 @@ setup(
     long_description_content_type = 'text/markdown',
 
     py_modules       = [ 'indexed_zstd' ],
-    libraries        = [ zstd_seek ] if platform == "darwin" else [],
+    libraries        = [ zstd_seek ],
     ext_modules      = extensions
 )
+
