1b574e67a045c06d9f00866b668c9342ea417231
Marco <6640057+martinellimarco@users.noreply.github.com>
2022-01-02 19:15:07 +0100

    Replaced travis with github actions. Adding support for building wheels for MacOS.

--- setup.py
+++ setup.py
@@ -51,7 +51,6 @@ setup(
                          'Operating System :: MacOS',
                          'Operating System :: POSIX',
                          'Operating System :: Unix',
-                         'Operating System :: Microsoft :: Windows',
                          'Programming Language :: Python :: 3',
                          'Programming Language :: Python :: 3.6',
                          'Programming Language :: Python :: 3.7',
