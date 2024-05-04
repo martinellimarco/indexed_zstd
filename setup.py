4704fea0271b860218bc766be4e0a5fa693864e4
Marco Martinelli <6640057+martinellimarco@users.noreply.github.com>
2024-05-04 23:39:57 +0200

    Update setup.py
    Python 3.12

--- setup.py
+++ setup.py
@@ -80,6 +80,7 @@ setup(
                          'Programming Language :: Python :: 3.9',
                          'Programming Language :: Python :: 3.10',
                          'Programming Language :: Python :: 3.11',
+                         'Programming Language :: Python :: 3.12',
                          'Programming Language :: C++',
                          'Programming Language :: C',
                          'Topic :: Software Development :: Libraries',
